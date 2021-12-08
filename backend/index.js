1bda160202db96d9d4b4dbd36fa4fc73cee96e6a
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-08 08:16:36 -0300

    Connect to MONGODB

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,16 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 const app = express();
+
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, 
+  {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => {
+    console.log('connected to MONGODB');  
+  }
+);
+
 app.listen(8800, () => {
-  console.log("backend is running on http://localhost/8800");
+  console.log('backend is running on http://localhost/8800');
 });
