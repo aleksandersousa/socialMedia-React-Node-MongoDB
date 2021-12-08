9d6b20da5622fecc98d5d4baa1b04bca8e5ae8f8
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-08 11:59:15 -0300

    Add hashed password

--- backend/index.js
+++ backend/index.js
@@ -4,19 +4,19 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-const userRoute = require('./routes/users')
-const authRoute = require('./routes/auth')
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 const app = express();
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, 
-  {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => {
-    console.log('connected to MONGODB');  
-  }
-);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MONGODB');
+  }).catch((err) => {
+    console.log(err);
+  });
 
 //middleware
 app.use(express.json());
@@ -24,8 +24,8 @@ app.use(helmet());
 app.use(morgan('common'));
 
 app.use('/api/users', userRoute);
-app.use('/api/auth', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
-  console.log('backend is running on http://localhost/8800');
+  console.log('backend is running on http://localhost:8800');
 });
