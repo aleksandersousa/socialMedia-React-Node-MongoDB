89fa03a70370e7c91725a12c8612ee4640332f00
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-08 08:19:16 -0300

    Add middleware

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,11 @@ mongoose.connect(process.env.MONGO_URL,
   }
 );
 
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
 app.listen(8800, () => {
   console.log('backend is running on http://localhost/8800');
 });
