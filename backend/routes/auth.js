065e69146fcf8e369d7aed3d83830be9e3c01664
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-08 12:12:05 -0300

    Add login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,4 +20,24 @@ router.post('/register', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json({
+      error: true,
+      message: 'user not found'
+    });
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    !validPassword && res.status(400).json({
+      error: true,
+      message: 'wrong password'
+    });
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
