a9c42a36fdf599784cdbd854fd625c68693c6727
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-08 13:19:18 -0300

    Add get user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -49,4 +49,14 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
