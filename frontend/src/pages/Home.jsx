149ba02a67c842d4db1feb0cbeceeb60b499e5f0
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-11 18:17:58 -0300

    Added sidebar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+
 import Topbar from '../components/topbar/Topbar';
+import Sidebar from '../components/sidebar/Sidebar';
+import Feed from '../components/feed/Feed';
+import Rightbar from '../components/rightbar/Rightbar';
 
 export default function Home() {
   return (
     <div className="home">
       <Topbar />
+      <body className="homeBody">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </body>
     </div>
   );
 }
