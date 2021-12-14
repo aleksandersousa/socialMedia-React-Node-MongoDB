1f071285dc8d900e8ddf4d31e96d888aaeb20bde
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-14 09:59:44 -0300

    Added like/dislike functionality

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Post.css';
 
 import { MoreVert } from '@material-ui/icons';
 import { Users } from '../../dummyData';
 
 export default function Post({ post }) {
+  const [ like, setLike ] = useState(post.like);
+  const [ isLiked, setIsLiked ] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  }
+
   return(
     <div className="post">
       <div className="postWrapper">
@@ -24,9 +32,9 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="/assets/like.png" alt="" className="likeIcon" />
-            <img src="/assets/heart.png" alt="" className="likeIcon" />
-            <span className="postLikeCounter">{post.like} people like it</span>
+            <img src="/assets/like.png" onClick={likeHandler} alt="" className="likeIcon" />
+            <img src="/assets/heart.png" onClick={likeHandler} alt="" className="likeIcon" />
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
