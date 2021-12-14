71c9f3fd32c2fa34471b368a2218669d65852c58
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-14 10:03:35 -0300

    Fixed home directory

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Home.css';
 
-import Topbar from '../components/topbar/Topbar';
-import Sidebar from '../components/sidebar/Sidebar';
-import Feed from '../components/feed/Feed';
-import Rightbar from '../components/rightbar/Rightbar';
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
 
 export default function Home() {
   return (
