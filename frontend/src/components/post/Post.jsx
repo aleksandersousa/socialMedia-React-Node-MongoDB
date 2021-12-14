c7b8d58ee06d9c2016f9515fde02012ba9683215
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-14 09:33:07 -0300

    using dynamic data

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -2,33 +2,34 @@ import React from 'react';
 import './Post.css';
 
 import { MoreVert } from '@material-ui/icons';
+import { Users } from '../../dummyData';
 
-export default function Post() {
+export default function Post({ post }) {
   return(
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
-            <span className="postUsername">Natália</span>
-            <span className="postDate">5 min ago</span>
+            <img src={Users.filter(u => u.id === post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+            <span className="postUsername">{Users.filter(u => u.id === post.userId)[0].username}</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert className="postIcon" />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">Hey! It's my first post</span>
-          <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+          <span className="postText">{post?.desc}</span>
+          <img src={post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img src="/assets/like.png" alt="" className="likeIcon" />
             <img src="/assets/heart.png" alt="" className="likeIcon" />
-            <span className="postLikeCounter">32 people like it</span>
+            <span className="postLikeCounter">{post.like} people like it</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">9 comments</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>
