9c1ae5d236f13eeaedbfdb021952912b77370445
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-27 12:15:40 -0300

    fixed user storage bug

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Home.css';
 
 import Topbar from '../../components/topbar/Topbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Feed from '../../components/feed/Feed';
 import Rightbar from '../../components/rightbar/Rightbar';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Home() {
+  const { authActions } = useContext(AuthContext);
+  window.addEventListener('beforeunload', () => {
+    authActions.logout();
+  });
+
   return (
     <div className="home">
       <Topbar />
