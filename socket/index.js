26ac31e96c8972c0d8897de97a7c8d8ad5998dd8
aleksandersousa <aleksander-ssousa@outlook.com>
2021-12-28 16:52:32 -0300

    Added real time messages logic

--- socket/index.js
+++ socket/index.js
@@ -15,6 +15,10 @@ const removeUser = (socketId) => {
   users = users.filter(user => user.socketId !== socketId);
 };
 
+const getUser = (userId) => {
+  return users.find(user => user.userId === userId);
+};
+
 io.on('connection', (socket) => {
   console.log("a user connected.");
 
@@ -22,6 +26,12 @@ io.on('connection', (socket) => {
   socket.on('addUser', (userId) => {
     addUser(userId, socket.id);
     io.emit('getOnlineUsers', users);
+  });
+
+  //send and get message
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit('getMessage', { senderId, text });
   });
 
   //when disconnect
